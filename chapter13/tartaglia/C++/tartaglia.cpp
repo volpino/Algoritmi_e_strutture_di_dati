d5d2314400d6e01b3bbf2efd9c3203e4e0b519b6
volpino <fox91@anche.no>
2013-08-08 14:19:06 +0200

    Adding tartaglia implementation

---
+++ chapter13/tartaglia/C++/tartaglia.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+#define DIM 1000
+
+long long C[DIM][DIM];
+
+
+void tartaglia(int n) {
+  for (int i=0; i<n; i++) {
+    C[i][0] = 1;
+    C[i][i] = 1;
+  }
+
+  for (int i=2; i<n; i++) {
+    for (int j=1; j<i; j++) {
+      cout << "i=" << i << " j=" << j << endl;
+      cout << C[i-1][j-1] + C[i-1][j] << endl;
+      C[i][j] = C[i-1][j-1] + C[i-1][j];
+    }
+  }
+}
+
+
+int main() {
+  tartaglia(10);
+
+  for (int i=0; i<10; i++) {
+    for (int j=0; j<10; j++) {
+      cout << "C(" << i << "," << j << ") = " << C[i][j] << endl;
+    }
+  }
+}
