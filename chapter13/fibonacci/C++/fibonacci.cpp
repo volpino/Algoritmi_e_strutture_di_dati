4900a93edb732eb38489c3ed6bd0c7ee713c1416
volpino <fox91@anche.no>
2013-08-12 10:27:25 +0200

    Adding Fibonacci in O(n) in memory and time

---
+++ chapter13/fibonacci/C++/fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int fibonacci(int n) {
+  int f0, f1, f2;
+  f0 = f1 = f2 = 1;
+
+  for (int i=2; i<n; i++) {
+    f0 = f1;
+    f1 = f2;
+    f2 = f0 + f1;
+  }
+
+  return f2;
+}
+
+
+int main(int argc, char** argv) {
+  if (argc <= 1) {
+    cout << "Usage: " << argv[0] << " N" << endl;
+    return 1;
+  }
+
+  cout << fibonacci(atoi(argv[1])) << endl;
+  return 0;
+}
---
+++ chapter13/fibonacci/Java/Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci {
+    public static int compute(int n) {
+        int f0, f1, f2;
+        f0 = f1 = f2 = 1;
+
+        for (int i=2; i<n; i++) {
+            f0 = f1;
+            f1 = f2;
+            f2 = f0 + f1;
+        }
+
+        return f2;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: java Fibonacci N");
+        }
+        else {
+            System.out.println(Fibonacci.compute(Integer.parseInt(args[0])));
+        }
+    }
+}
